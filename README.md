985c3b93f0bb01775912e99080f27fd05a9683d4
davidhin <hello@davidhin.com>
2022-04-27 21:09:23 +0930

    Final update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MAL-sorter
+
+## Quickstart
+
+Assume conda/mamba are already installed.
+
+```sh
+mamba env create -f environment.yml --force
+conda activate mal-sorter
+python main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """MAL script for sorting anime by last finished."""
+from datetime import datetime
 import json
 import os
+from pathlib import Path
+import re
 import secrets
 
 import pandas as pd
@@ -25,6 +28,7 @@ class MyAnimeList:
                 self.token = json.load(file)
         else:
             self._authorize()
+        self.alist = mal.get_animelist()
 
     async def _auth_url(self):
         """Print the URL needed to authorise your application."""
@@ -76,6 +80,18 @@ class MyAnimeList:
         response.raise_for_status()
         return response
 
+    def patch(self, url, data):
+        """Put data to MAL."""
+        headers = {"Authorization": f"Bearer {self.token['access_token']}"}
+        response = requests.patch(url, data=data, headers=headers)
+        return response
+
+    def patch_anime(self, anime_id, data):
+        """Update anime data."""
+        return self.patch(
+            f"https://api.myanimelist.net/v2/anime/{anime_id}/my_list_status", data,
+        )
+
     def get_stats(self):
         """Get user stats."""
         url = "https://api.myanimelist.net/v2/users/@me?fields=anime_statistics"
@@ -90,7 +106,8 @@ class MyAnimeList:
         """Get anime list from MAL as dataframe."""
         num_anime = self.get_number_of_anime()
         animelist = []
-        url = "https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status&limit=100&nsfw=true"
+        params = "?fields=list_status&limit=100&nsfw=true"
+        url = f"https://api.myanimelist.net/v2/users/@me/animelist{params}"
         while True:
             response = mal.get(url).json()
             animelist += response["data"]
@@ -115,10 +132,74 @@ class MyAnimeList:
         updates = [i.text.replace(" Remove", "") for i in updates]
         return {"anime_title": anime_title, "anime_id": anime_id, "updates": updates}
 
+    def format_title(title: str) -> str:
+        """Format title into filename safe string."""
+        return re.sub(r"[^A-Za-z0-9\ ]+", "", title).replace(" ", "_")
+
+    def cache_anime_update_history(self, anime_id: int, outfile: str):
+        """Call get anime update history with file system caching."""
+        if not os.path.exists(outfile):
+            with open(outfile, "w") as f:
+                info = self.get_anime_update_history(anime_id)
+                json.dump(info, f, indent=4)
+                print(f"Saved to {outfile}")
+
+    def cache_completed_histories(self):
+        """Cache completed histories."""
+        completed = self.alist[self.alist.status == "completed"]
+        for anime_id, title in completed[["id", "title"]].values.tolist():
+            title = MyAnimeList.format_title(title)
+            Path("cache").mkdir(exist_ok=True)
+            outfile = f"cache/{anime_id}.json"
+            mal.cache_anime_update_history(anime_id, outfile)
+        print("Cached watch histories!")
+
+    def dmY_to_Ymd(self, date: str):
+        """Convert 08/10/2018 to 2018-10-08."""
+        return datetime.strptime(date, "%m/%d/%Y").strftime("%Y-%m-%d")
+
+    def get_start_finish_dates(self):
+        """Return list of records with anime_id, start_date, and finish_date."""
+        d = "cache/"
+        ret = []
+        for fname in os.listdir("cache/"):
+            with open(os.path.join(d, fname), "r") as f:
+                data = json.load(f)
+                ret.append(
+                    {
+                        "id": data["anime_id"],
+                        "start_date": self.dmY_to_Ymd(data["updates"][-1].split()[4]),
+                        "finish_date": self.dmY_to_Ymd(data["updates"][0].split()[4]),
+                    }
+                )
+        return ret
+
+    def get_reorder_df(self):
+        """Get reorder dataframe."""
+        df = pd.DataFrame.from_records(self.get_start_finish_dates())
+        df["sort"] = pd.to_datetime(df.finish_date)
+        df = df.sort_values("sort")
+        df = df.merge(self.alist[["id", "score", "title"]], on="id")
+        return df
+
+    def reorder_by_finished_date(self):
+        """Reorder completed by finish date.
+
+        Also adds start/finish date based on update history.
+        Does not account for rewatch; sets finish date as most recent
+        finish date.
+        """
+        for i in mal.get_reorder_df().itertuples():
+            anime_id = i.id
+            metadata = {
+                "score": i.score,
+                "start_date": i.start_date,
+                "finish_date": i.finish_date,
+            }
+            print(mal.patch_anime(anime_id, metadata).text)
+
 
 if __name__ == "__main__":
     mal = MyAnimeList()
-
-    # WIP
-    # mal.get_animelist()
-    # mal.get_anime_update_history(48553)
+    mal.cache_completed_histories()
+    mal.reorder_by_finished_date()
